bb3d4e171dd1de2f2d55aff94224c127728dca4c
liviaerxin <1yue8haogaoqi@gmail.com>
2015-07-03 00:47:14 +0800

    modify detail-form.js with post form each item at one time

--- detail-form.js
+++ detail-form.js
@@ -1,6 +1,44 @@
 $(document).ready(function(){
+	/*form submit only one form with all items*/
+	/*
 	$("#commit-btn").click(function(){
       alert("333");
       $("#items").submit();
     });
+	*/
+    
+    /*AJAX to post one form with one item  */
+	$("#commit-btn").click(function(){
+		/*
+    	$.post("http://127.0.0.1:7000/commit",
+    		{suggest: "axn"},
+    		function(data,status){
+    			alert("Data: " + data + "\nStatus: " + status);
+    		}
+    		);
+    	*/
+    	var base_url = "http://127.0.0.1:7000/commit/";
+    	$(".tab-content form").each(function(){
+    		var data=$(this).serialize();
+    		var item=$(this).parent().attr("id");
+    		alert("Post item: " + item + "\nData: " + data);
+
+    		$.post(
+    			base_url + item,
+    			data,
+    			function(data,status){
+    				alert("Data: " + data + "\nStatus: " + status);
+    			}
+    		);
+    	});
+
+    });
+	
+
+	$("#items").attr("onsubmit", "return validate_form(this)");
+
 })
+var validate_form = function(thisform){
+		alert("bbbb");
+		return true;
+	};
--- server.py
+++ server.py
@@ -3,14 +3,18 @@
 @author: siyao
 industries finance data interface, response is JSON style
 """
-from bottle import route, request, run
+from bottle import route, request, run, response, hook
 
-@route("/commit",method="post")
-def commit():
+@hook('after_request')
+def enable_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+@route("/commit/<item>",method="post")
+def commit(item=None):
     for k,v in request.forms.items():
         print k,v
         print "\n"
-    return "hello"
+    return "Item: " + item + "Committed Successed" 
 	
 if __name__ == '__main__':
     run(host = "127.0.0.1", port = 7000, debug = True)
